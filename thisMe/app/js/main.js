8ca4e749180b0b978b0d28e2dec9a59290011f7e
Achmad Hafid Supriyadi <43329305+MrHafid@users.noreply.github.com>
2022-07-20 01:16:01 +0700

    add thisMe LinkTree

---
+++ thisMe/app/js/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-input",{
+    strings:["Programmer.","Flutter Developer.","Frontend Flutter Mobile.","Freelancer."],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+})
