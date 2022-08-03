c6d4c5e8a9c12b74d9c1416928d9a87aa46de751
Achmad Hafid Supriyadi <43329305+MrHafid@users.noreply.github.com>
2022-08-03 22:13:58 +0700

    Update register.js

--- bottom_menu/register.js
+++ bottom_menu/register.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
     if ('serviceWorker' in navigator && navigator.onLine) {
-        navigator.serviceWorker.register('./sw3.js')
+        navigator.serviceWorker.register('https://mrhafid.github.io/bottom_menu/sw3.js')
         .then((reg) => {
             console.log('Registrasi service worker Berhasil', reg);
         }, (err) => {
